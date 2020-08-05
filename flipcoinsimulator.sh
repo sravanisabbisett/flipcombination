a6dbedd13fc06a088e841d1b638115be12d5d87f
sravanisabbisett <sravani.sabbisetti1@gmail.com>
2020-08-05 10:02:25 +0530

    uc2 added

--- flipcoinsimulator.sh
+++ flipcoinsimulator.sh
@@ -2,6 +2,7 @@
 
 head=1
 tail=0
+num=1
 randomCheck=$(( RANDOM%2 ))
 if [ $randomCheck -eq $head ]
 then
@@ -9,3 +10,29 @@ then
 else
 	echo "coin flips tail"
 fi
+
+while [ $num -le 10 ]
+do
+	var=$(( RANDOM%2 ))
+	if [ $var -eq $head ]
+	then
+		flip="H"
+		count1=$(( $count1+1 ))
+	else
+		flip="T"
+		count2=$(( $count2+1 ))
+	fi
+	singlet[$num]=$flip
+	num=$(( $num+1 ))
+	echo ${singlet[@]}
+done
+
+len=${#singlet[@]}
+headper=$(echo | awk '{print '$count1/$len*100'}')
+echo "winning percentage of head: " $headper
+
+tailper=$(echo | awk '{print '$count2/$len*100'}')
+echo "winning percentage of tail: " $tailper
+
+array1=($headper $tailper)
+echo ${array1[@]}
