ae88c6403f8847869186aca9b003a1e0a1a48b49
sravanisabbisett <sravani.sabbisetti1@gmail.com>
2020-08-05 10:09:51 +0530

    uc3 added

--- flipcoinsimulator.sh
+++ flipcoinsimulator.sh
@@ -3,6 +3,7 @@
 head=1
 tail=0
 num=1
+num1=1
 randomCheck=$(( RANDOM%2 ))
 if [ $randomCheck -eq $head ]
 then
@@ -36,3 +37,45 @@ echo "winning percentage of tail: " $tailper
 
 array1=($headper $tailper)
 echo ${array1[@]}
+
+while [ $num1 -le 10 ]
+do
+   var=$(( RANDOM%2 ))
+   var1=$(( RANDOM%2 ))
+   if [[ $var -eq $head && $var1 -eq $head ]]
+   then
+      flip="HH"
+		count3=$(( $count3+1 ))
+   elif [[ $var -eq $head && $var1 -eq $tail ]]
+   then
+      flip="HT"
+		count4=$(( $count4+1 ))
+   elif [[ $var -eq $tail && $var1 -eq $head ]]
+   then
+      flip="TH"
+		count5=$(( $count5+1 ))
+   else [[ $var -eq $tail && $var1 -eq $head ]]
+      flip="TT"
+		count6=$(( $count6+1 ))
+   fi
+   doublet[$num1]=$flip
+   num1=$(( $num1+1 ))
+   echo ${doublet[@]}
+done
+
+len1=${#doublet[@]}
+
+HH=$(echo | awk '{print '$count3/$len1*100'}')
+echo "winning percentage for combination HH" $HH
+
+HT=$(echo | awk '{print '$count4/$len1*100'}')
+echo "winning percentage for combination HT" $HT
+
+TH=$(echo | awk '{print '$count5/$len1*100'}')
+echo "winning percentage for combination TH" $TH
+
+TT=$(echo | awk '{print '$count6/$len1*100'}')
+echo "winning percentage for combination TT" $TT
+
+array2=($HH $HT $TH $TT)
+echo ${array2[@]}
